46b5c55b8c70eb5eb09fac49a7f52099219907f8
Nowl2112 <s10258636@connect.np.edu.sg>
2024-06-07 17:16:57 +0800

    building class

---
+++ SPM Assignment Ngee Ann City/SPM Assignment Ngee Ann City/Building.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPM_Assignment_Ngee_Ann_City
+{
+    class Building
+    {
+        private string type;
+        private int points;
+        private string position;
+
+        public string Type { get; set; }
+        public int Points { get; set; }
+        public string Position { get; set; }
+        public Building(string type, int points, string position)
+        {
+            Type = type;
+            Points = points;
+            Position = position;
+        }
+        public override string ToString()
+        {
+            return "Type: " + type + "\tPoints: " + points + "\tPosition" + position;
+        }
+
+    }
+}

02a833dff7f42a84d8e8ba24968f5904e170496b
TeoYM <150310107+TeoYM@users.noreply.github.com>
2024-06-09 11:25:27 +0800

    Add files via upload

---
+++ SPM Assignment Ngee Ann City/Remove.cs
@@ -0,0 +1,19 @@
+﻿public class BuildingManager
+{
+    private List<Building> buildings = new List<Building>();
+
+    public void AddBuilding(Building building)
+    {
+        buildings.Add(building);
+    }
+
+    public void RemoveBuilding(Building building)
+    {
+        buildings.Remove(building);
+    }
+
+    public List<Building> GetBuildings()
+    {
+        return buildings;
+    }
+}

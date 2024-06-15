9658a8381db0ef25a4a7bea8d46ab62835e30d19
LauYanHui <S10241639@connect.np.edu.sg>
2024-06-15 23:10:28 +0800

    edit

--- SPM Assignment Ngee Ann City/SPM Assignment Ngee Ann City/Program.cs
+++ SPM Assignment Ngee Ann City/SPM Assignment Ngee Ann City/Program.cs
@@ -140,69 +140,46 @@ void displayBuildingTypes()
     Console.WriteLine("[5] Road");
 }
 
-/*
-char[] letters = "ABCDEFGHIJKLMNOPQRST".ToCharArray();
-Console.Write(" ");
-Console.WriteLine("    " + string.Join("   ", letters)); // Print the letters with spaces and dashes
-Console.Write(" ");
-Console.WriteLine("  +" + new string('-', 4 * letters.Length - 1)+ "+"); 
-
-for (int i = 0; i < 20; i++)
+void DisplayLeaderboard()
 {
-    Console.Write($"{i + 1,2} |"); // Print the row number and vertical bar
-    for (int j = 0; j < 20; j++)
+    Console.WriteLine("Leaderboard" + "\n");
+
+    SortedDictionary<User, int> userDict = new SortedDictionary<User, int>();
+    User user0 = new User("test12120", "password0", false, 0);
+    User user1 = new User("test1", "password1", false, 20);
+    User user2 = new User("test2", "password2", false, 90);
+    User user3 = new User("test3", "password3", false, 100);
+    User user4 = new User("test4", "password4", false, 50);
+    User user5 = new User("test5", "password5", false, 60);
+    User user6 = new User("test6", "password6", false, 70);
+    User user7 = new User("test7", "password7", false, 75);
+    User user8 = new User("test8", "password8", false, 10);
+    User user9 = new User("test9", "password9", false, 30);
+
+
+
+    userDict.Add(user0, user0.Points);
+    userDict.Add(user1, user1.Points);
+    userDict.Add(user2, user2.Points);
+    userDict.Add(user3, user3.Points);
+    userDict.Add(user4, user4.Points);
+    userDict.Add(user5, user5.Points);
+    userDict.Add(user6, user6.Points);
+    userDict.Add(user7, user7.Points);
+    userDict.Add(user8, user8.Points);
+    userDict.Add(user9, user9.Points);
+
+    Console.WriteLine("--------------------------------");
+    Console.WriteLine("\tName" + "           Points");
+    Console.WriteLine("--------------------------------");
+    int count = 1;
+    foreach (KeyValuePair<User, int> kvp in userDict)
     {
-        Console.Write("   |"); // Print the spaces and vertical bars
-
+        Console.WriteLine(String.Format("{0}:\t{1,-15}{2}", count, kvp.Key.Name, kvp.Value));
+        count++;
     }
-    Console.WriteLine();
-    Console.Write(" ");
-    Console.WriteLine("  +" + new string('-', 4 * letters.Length - 1) + "+");
+    Console.WriteLine("--------------------------------");
 }
-Console.WriteLine("///////////////////////////////////////////////////////////////////");
-// Initialize a 2D list of characters
-List<List<char>> grid = new List<List<char>>();
-for (int i = 0; i < 20; i++)
-{
-    grid.Add(new List<char>(new char[20])); // Initialize each row with empty characters
-}
-Console.Write("Enter a coordinate: ");
-string userInput = Console.ReadLine();
-input(userInput);
-void input(string input)
-{
-    string userInput = input;
-    char rowLetter = userInput[0];
-    int columnIndex = int.Parse(userInput.Substring(1)) - 1;
-
-    
-    int rowIndex = rowLetter - 'A';
-
-    
-    char desiredLetter = 'X';
-    grid[rowIndex][columnIndex] = desiredLetter;
-
-    
-    Console.Write(" ");
-    Console.WriteLine("    " + string.Join("   ", letters));
-    Console.Write(" ");
-    Console.WriteLine("  +" + new string('-', 4 * letters.Length - 1) + "+");
-
-    for (int i = 0; i < 20; i++)
-    {
-        Console.Write($"{i + 1,2} |");
-        for (int j = 0; j < 20; j++)
-        {
-            char cellValue = grid[j][i];
-            string formattedValue = cellValue == '\0' ? "   " : $" {cellValue} ";
-            Console.Write(formattedValue + "|");
-        }
-        Console.WriteLine();
-        Console.Write(" ");
-        Console.WriteLine("  +" + new string('-', 4 * letters.Length - 1) + "+");
-    }
-}
-(char, int)[,] newgrid = new (char, int)[20, 20];
-*/
+DisplayLeaderboard();
 
 
